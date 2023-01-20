2a5354156e45e189add38104edf1c308f270a7e2
AswathiMohan23 <achu999here@gmail.com>
2023-01-20 10:10:24 +0530

    [ASWATHI] refactored . addressbook code using class concept

--- AddressBook_OOPS/AddressBook/Address_book_methods.py
+++ AddressBook_OOPS/AddressBook/Address_book_methods.py
@@ -75,20 +75,3 @@ class Address_book_methods(object):
 
 
 # ------------------------------------------------------------------------------------------------------------------------
-
-print("\n\t\t\t\t\t\t\t\t\t WELCOME TO ADDRESSBOOK \n\t\t\t\t\t\t\t\t  ***************************\n")
-
-contact = []
-addressBook = {}
-address = Address_book_methods(contact,addressBook)
-address.adding_data_from_console(contact)
-address.display_contactBook(contact)
-address.edit_data(contact)
-address.display_contactBook(contact)
-address.delete_contact(contact)
-print("\n\n printing list using iterator : ")
-length = len(contact)
-iterator = iter(contact)
-print(next(iterator))
-address.multiple_addressBook(addressBook)
-address.display_contactBook(addressBook)
